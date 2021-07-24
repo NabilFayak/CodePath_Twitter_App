3a9cc7300992a72fab2a6a28cddc863ec7e80309
Nabil Fayak <nabil.fayak@baruchmail.cuny.edu>
2021-07-23 20:47:24 -0400

    Initial Commit

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Nabil Fayak on 7/23/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    @IBOutlet weak var ProfileImageView: UIImageView!
+    @IBOutlet weak var UserNameLabel: UILabel!
+    @IBOutlet weak var TweetContent: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
